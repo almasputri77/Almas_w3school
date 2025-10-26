8d4b79f48a50755c81e2227fd3453cb50dbfa03f
Almas <almasputri77@gmail.com>
2025-10-26 14:21:10 +0800

    update

---
+++ 22Setting the Data Type.py
@@ -0,0 +1,23 @@
+# x = "Hello World"	                         str        (string)    = text
+# x = 20	                                 int	    (integer)   = number
+# x = 20.5	                                 float	                = number, decimal
+# x = 1j	                                 complex	
+# x = ["apple", "banana", "cherry"]	         list	
+# x = ("apple", "banana", "cherry")	         tuple	
+# x = range(6)	                             range	
+# x = {"name" : "John", "age" : 36}	         dict	    (dictionary) = struktur data yang menyimpan pasangan key dan value
+# x = {"apple", "banana", "cherry"}	         set	
+# x = frozenset({"apple", "banana", "cherry"})	frozenset	
+# x = True	                                 bool	    (boolean)    = tipe data yang hanya punya dua nilai kemungkinan
+# x = b"Hello"	                             bytes	
+# x = bytearray(5)	                         bytearray	
+# x = memoryview(bytes(5))	                 memoryview	
+# x = None	                                 NoneType
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
