a250138db906e75463124fb934421858eae79b0f
Almas <almasputri77@gmail.com>
2025-10-28 10:09:55 +0800

    update

---
+++ 41Pyton Tuples - Tuple Method.py
@@ -0,0 +1,12 @@
+# Tuple Methods
+# Python has two built-in methods that you can use on tuples.
+
+# Method	Description
+# count()	Returns the number of times a specified value occurs in a tuple
+# index()	Searches the tuple for a specified value and returns the position of where it was found
+
+angka = (1, 2, 3, 2, 4, 2, 5)
+print(angka.count(2))
+
+buah = ("apel", "pisang", "ceri", "pisang")
+print(buah.index("pisang"))
